45e5b052241fdb19b90f6af5bc4424d26536a4a0
Meng Li <34143965+Meng6@users.noreply.github.com>
2023-06-30 16:09:02 -0500

    Add create, destroy, export, graph, query

---
+++ demo/02-rdflib-demos/run.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------
+
+bash_cell available_commands << END_CELL
+
+../../geist --help
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell create_command << END_CELL
+
+../../geist create --help
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell create_dataset_test << END_CELL
+
+../../geist create -d test -ifile data/tro.jsonld
+
+ls .geistdata
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell destroy_command << END_CELL
+
+../../geist destroy --help
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell destroy_dataset_test << END_CELL
+
+../../geist destroy -d test
+
+ls .geistdata
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell create_dataset_kb << END_CELL
+
+../../geist create -ifile data/tro.jsonld
+
+ls .geistdata
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell export_command << END_CELL
+
+../../geist export --help
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell export_dataset_kb << END_CELL
+
+../../geist export
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell graph_command << END_CELL
+
+../../geist graph --help
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell graph_dataset_kb << END_CELL
+
+../../geist graph -m data/mappings.json -ofile products/kb.png
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell query_command << END_CELL
+
+../../geist query --help
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell query_dataset_kb << END_CELL
+
+../../geist query --file data/query
+
+END_CELL
+
+# ------------------------------------------------------------------------------
